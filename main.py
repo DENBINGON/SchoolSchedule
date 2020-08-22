c1e2f5d9ff86d842dac7a47a551c30c2de416d48
denbingon <denbingon@gmail.com>
2020-08-22 22:49:51 +0300

    Некоторые изменения, отказ от секретного ключа и ключа пользователя

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # -*- utf-8 -*-
 
 # Импорт библиотек
-import vk_api, random
-from modules import const, db, sys, userAuthMethods
+import vk_api, random, vk
+from modules import const, db
 from modules.bot import VkBot
 from vk_api.longpoll import VkLongPoll, VkEventType
 
@@ -11,10 +11,15 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 # Основной класс
 class SchoolScheduleMainApp( ):
     def __init__ ( self ):
+
         self.session = vk_api.VkApi( token=const.__TOKEN_APP__ )
+        self.session_ = vk.Session( access_token=const.__TOKEN_APP__ )
         self.session._auth_token( )
+        self.API = vk.API( self.session_ )
+
         self.longpoll = VkLongPoll( self.session )
-        self.userMethods, self.DBMethods = userAuthMethods.UserAuth( ), db.dataBaseMethods( )
+        self.DBMethods = db.dataBaseMethods( )
+
         for event in self.longpoll.listen( ):
             if event.type == VkEventType.MESSAGE_NEW:
                 if event.to_me:
@@ -27,12 +32,12 @@ class SchoolScheduleMainApp( ):
                         if self.DBMethods.checkUser( event.user_id ) == True:
                             self.DBMethods.removeUser( event.user_id )
                             self.DBMethods.addNewUserInformation(
-                                self.userMethods.getUserInformation( event.user_id )[ 0 ] )
+                                self.getUserInformation( event.user_id )[ 0 ] )
                             answer, userCode = bot.MessageParcer( event.text )
                             self.WriteMessage( event.user_id, answer )
                         else:
                             self.DBMethods.addNewUserInformation(
-                                self.userMethods.getUserInformation( event.user_id )[ 0 ] )
+                                self.getUserInformation( event.user_id )[ 0 ] )
                             answer, userCode = bot.MessageParcer( event.text )
                             self.WriteMessage( event.user_id, answer )
 
@@ -41,6 +46,8 @@ class SchoolScheduleMainApp( ):
                              { 'user_id': user_id, 'message': message, 'random_id': random.random( ) * 1000,
                                'keyboard': const.__KEYBOARD__ } )
 
+    def getUserInformation ( self, user_id ):
+        return self.API.users.get( user_id=user_id, v='5.21' )
 
 # Запускаем
 if __name__ == "__main__":
--- modules/const.py
+++ modules/const.py
@@ -1,6 +1,4 @@
 __TOKEN_APP__ = ""
-__TOKEN_USER__ = ""
-__SECRET_KEY__ = ""
-__version__ = "V0.1[R0.1] OPEN BETA"
+__version__ = "V0.1[R1.1] OPEN BETA"
 __KEYBOARD__ = '{"one_time":false,"buttons":[[{"action":{"type":"text","label":"Расписание звонков","payload":""},"color":"secondary"},{"action":{"type":"text","label":"Расписание уроков","payload":""},"color":"secondary"}],[{"action":{"type":"text","label":"Что на завтра","payload":""},"color":"primary"},{"action":{"type":"text","label":"Что на","payload":""},"color":"primary"}],[{"action":{"type":"text","label":"Помощь","payload":""},"color":"positive"}]]}'
-__dataBase_Download__ = 'https://github.com/DENBINGON/SchoolSchedule/raw/dev/modules/database/db_null.sqlite'
+__dataBase_Download__ = 'https://github.com/DENBINGON/SchoolSchedule/raw/master/modules/database/db_null.sqlite'
