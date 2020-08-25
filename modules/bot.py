700414a1faa7eca36fba0d58d19b92536ab3f5f9
denbingon <denbingon@gmail.com>
2020-08-25 14:56:50 +0300

    Оптимизация

--- modules/bot.py
+++ modules/bot.py
@@ -15,7 +15,7 @@ class VkBot:
         elif message.upper( ) == self._COMMANDS[ 2 ]:
             return dev, 1
         elif message.upper( ) == self._COMMANDS[ 3 ]:
-            return scheduleInformation( ), 1
+            return scheduleInformation, 1
         elif message.upper( ) == self._COMMANDS[ 4 ]:
             return dev, 1
         elif message.upper( ) == self._COMMANDS[ 5 ]:
@@ -29,6 +29,6 @@ class VkBot:
         elif message.upper( ) == self._COMMANDS[ 7 ]:
             return help, 1
         elif message.upper( ) == self._COMMANDS[ 8 ]:
-            return scheduleCallsInformation( ), 1
+            return scheduleCallsInformation, 1
         else:
             return dontKnow, 1
